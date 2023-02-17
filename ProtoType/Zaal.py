e0ea4093d7e0d6784319b2c4cfc3705159bef25d
JeCheeseSmith <kars@duck.com>
2023-02-17 12:41:17 +0100

    full contract organised

---
+++ ProtoType/Zaal.py
@@ -0,0 +1,22 @@
+"""
+Deze ADT geeft een zaal weer
+
+data:
+self.zaalnummer: integer (nummer van de overeenkomstige zaal)
+self.plaatsen: integer (aantal plaatsen in de zaal)
+"""
+
+
+class Zaal:
+    def __init__(self, nummer, plaatsen):
+        """
+        Initialiseer een zaal met een nummer en een aantal plaatsen
+
+        precondities: Er worden 2 parameters gegeven, die beide integers zijn.
+                      De parameters zijn ook niet None.
+        postcondities: Er wordt een nieuwe zaal aangemaakt
+
+        :param nummer: integer
+        :param plaatsen: integer
+        """
+        pass
